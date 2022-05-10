87f2462405b09b39586d8f6a4f358572c137537d
Aerie <marenubium87@gmail.com>
2022-05-10 16:56:33 -0600

    Finished up to level 3 interview practice

--- Interview Practice/InterviewPractice 0429/InterviewPractice 0429/main.cpp
+++ Interview Practice/InterviewPractice 0429/InterviewPractice 0429/main.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
-#include <vector>
 #include <string>
+#include <vector>
 #include <fstream>
+#include <stack>
 
-using std::vector;
 using std::string;
-using std::cin;
-using std::cout;
-using std::fstream;
+using std::vector;
 using std::endl;
+using std::fstream;
+using std::getline;
+using std::cout;
+using std::stack;
 
-//rewrite to create a vector<vector<string>>
 //grabs queries from input file and parses them for solution fcn
 vector<vector<string>> parser(void) {
 	fstream input;
@@ -20,7 +21,7 @@ vector<vector<string>> parser(void) {
 	//temp vector that will be pushed into commands vector of vectors
 	vector<string> inputCommand;
 
-	input.open("input.txt", std::ios::in);
+	input.open("inputlvl2.txt", std::ios::in);
 
 	while (!input.eof()) {
 		inputCommand.clear();
@@ -32,6 +33,12 @@ vector<vector<string>> parser(void) {
 			std::getline(input, tempString);
 			inputCommand.push_back(tempString);
 		}
+		else if (tempString == "SELECT") {
+			std::getline(input, tempString);
+			inputCommand.push_back(tempString);
+			std::getline(input, tempString);
+			inputCommand.push_back(tempString);
+		}
 
 		commands.push_back(inputCommand);
 	}
@@ -39,103 +46,283 @@ vector<vector<string>> parser(void) {
 	return commands;
 }
 
+class TextEditor {
+public:
 
-void append(vector<string> & solution, string input, unsigned int & cursorPos) {
-	string tempString = "";
-	
-	//does a previous nonzero string exist to append to?
-	if (solution.size() > 0) {
-		tempString = solution[solution.size() - 1];
+	TextEditor();
+
+	//calls fcns based on command required in input
+	void process(vector<string> command);
+
+	vector<string> getSolution(void) const {
+		return solution;
 	}
 
-	tempString.insert(cursorPos, input);
-	solution.push_back(tempString);
+private:
 
-	//move cursor to end of added string
-	cursorPos += input.size();
+	class Command {
+	public:
+
+		Command(string inputString, int newCursorPos,
+			int newLeft, int newRight, int newActiveSelection);
+
+		string currentString;
+		int cursorPos;
+		int left;
+		int right;
+		bool activeSelection;
+
+	private:
+
+	};
+
+	vector<string> solution;
+	int cursorPos;
+	int left;
+	int right;
+	bool activeSelection;
+
+	string currentString;
+	string clipboard;
+
+	stack<Command> undoHistory;
+	stack<Command> redoHistory;
+
+	void prepareText (void);
+
+	void append(string input);
+	void move(string newCursorPos);
+	void backspace();
+	void select(string left, string right);
+	void copy();
+	void paste();
+	void undo();
+	void redo();
+
+};
+
+TextEditor::TextEditor() {
+	cursorPos = 0;
+	left = 0;
+	right = 0;
+	activeSelection = false;
+	currentString = "";
+	clipboard = "";
+}
+
+TextEditor::Command::Command(string inputString, int newCursorPos,
+		int newLeft, int newRight, int newActiveSelection) {
+
+	currentString = inputString;
+	cursorPos = newCursorPos;
+	left = newLeft;
+	right = newRight;
+	activeSelection = newActiveSelection;
+}
+
+void TextEditor::process(vector<string> command) {
+	
+	if (command[0] == "APPEND") {
+		//do appendy things involving command[1]
+		append(command[1]);
+		undoHistory.push(Command(currentString, cursorPos, left, right, 
+			activeSelection));
+		while (!redoHistory.empty()) {
+			redoHistory.pop();
+		}
+
+	}
+	else if (command[0] == "MOVE") {
+		//do movey things involving command[1]
+		move(command[1]);
+	}
+	else if (command[0] == "BACKSPACE") {
+		//do backspacey things
+		backspace();
+		undoHistory.push(Command(currentString, cursorPos, left, right,
+			activeSelection));
+		while (!redoHistory.empty()) {
+			redoHistory.pop();
+		}
+	}
+	else if (command[0] == "SELECT") {
+		//do selecty things involving command 1, 2
+		select(command[1], command[2]);
+	}
+	else if (command[0] == "COPY") {
+		//do copy things
+		copy();
+	}
+	else if (command[0] == "PASTE") {
+		//do pasty things
+		paste();
+		undoHistory.push(Command(currentString, cursorPos, left, right,
+			activeSelection));
+		while (!redoHistory.empty()) {
+			redoHistory.pop();
+		}
+	}
+	else if (command[0] == "UNDO") {
+		undo();
+	}
+	else if (command[0] == "REDO") {
+		redo();
+	}
+
+	solution.push_back(currentString);
+}
+
+void TextEditor::prepareText(void) {
+	
+	if (solution.size() > 0) {
+		currentString = solution[solution.size() - 1];
+	}
+}
+
+void TextEditor::append(string input) {
+
+	prepareText();
+
+	if (activeSelection) {
+		currentString.replace(left, right - left, input);
+		cursorPos = left + input.size();
+		activeSelection = false;
+	}
+	else {
+		currentString.insert(cursorPos, input);
+		cursorPos += input.size();
+	}
+}
+
+void TextEditor::move(string newCursorPos) {
+	prepareText();
+
+	cursorPos = stoi(newCursorPos);
+
+	if (cursorPos < 0) {
+		cursorPos = 0;
+	}
+	else if (cursorPos > (int) currentString.size()) {
+		cursorPos = currentString.size();
+	}
+
+	if (activeSelection) {
+		activeSelection = false;
+	}
 
 }
 
-void move(vector<string> & solution, string input, unsigned int & cursorPos) {
-	//convert input to number
-	cursorPos = std::stoi(input);
+void TextEditor::backspace(void) {
+	prepareText();
 
-	string tempString = "";
+	if (activeSelection) {
+		currentString.erase(left, right - left);
 
-	//does a previous nonzero string exist?
-	if (solution.size() > 0) {
-		tempString = solution[solution.size() - 1];
+		cursorPos = left;
+		activeSelection = false;
 	}
-	
-	solution.push_back(tempString);
+	else if (cursorPos > 0) {
+		currentString.erase(cursorPos - 1, 1);
+		cursorPos--;
+	}
 
-	//prevents cursor from being out of bounds
-	if (cursorPos > tempString.size()) {
-		cursorPos = tempString.size();
+}
+
+void TextEditor::select(string newLeft, string newRight) {
+	prepareText();
+
+	left = stoi(newLeft);
+	right = stoi(newRight);
+	if (left < 0) {
+		left = 0;
+	}
+	if (right > currentString.size()) {
+		right = currentString.size();
+	}
+
+	if (right > left) {
+		activeSelection = true;
+	}
+	else if (right == left) {
+		cursorPos = left;
+	}
+
+}
+
+void TextEditor::copy(void) {
+	prepareText();
+	if (activeSelection) {
+		clipboard = currentString.substr(left, right - left);
+	}
+
+}
+
+void TextEditor::paste(void) {
+	prepareText();
+
+	append(clipboard);
+}
+
+void TextEditor::undo(void) {
+	prepareText();
+
+	if (!undoHistory.empty()) {
+		redoHistory.push(undoHistory.top());
+		undoHistory.pop();
+
+		if (!undoHistory.empty()) {
+			Command & last = undoHistory.top();
+			currentString = last.currentString;
+			cursorPos = last.cursorPos;
+			left = last.left;
+			right = last.right;
+			activeSelection = last.activeSelection;
+		}
+		else {
+			currentString = "";
+			cursorPos = 0;
+			activeSelection = false;
+		}
 	}
 }
 
-void backspace(vector<string> & solution, unsigned int & cursorPos) {
-	string tempString = "";
+void TextEditor::redo(void) {
+	prepareText();
 
-	//does a previous nonzero string exist to delete from?
-	if (solution.size() > 0) {
-		tempString = solution[solution.size() - 1];
-	}
-	
-	if (cursorPos == 0) {
-		//can't delete if already at front
-		solution.push_back(tempString);
-		return;
-	}
-	
-	//forces erase (which is del behavior) into backsp behavior
-	tempString.erase(cursorPos - 1, 1);
-	solution.push_back(tempString);
+	if (!redoHistory.empty()) {
+		Command & next = redoHistory.top();
+		currentString = next.currentString;
+		cursorPos = next.cursorPos;
+		left = next.left;
+		right = next.right;
+		activeSelection = next.activeSelection;
 
-	cursorPos -= 1;
+		undoHistory.push(next);
+		redoHistory.pop();
+
+	}
 }
 
-//rewrite to accept a vector<vector<string>>
 vector<string> solution(vector<vector<string>> queries) {
 	
-	unsigned int cursorPos = 0;
-	vector<string> soln;
+	TextEditor myText;
+
 
 	for (vector<string> command : queries) {
-		//read next command
-		if (command[0] == "APPEND") {
-			append(soln, command[1], cursorPos);
-		}
-		if (command[0] == "MOVE") {
-			move(soln, command[1], cursorPos);
-		}
-		if (command[0] == "BACKSPACE") {
-			backspace(soln, cursorPos);
-		}
+		myText.process(command);
 	}
 
-	return soln;
+	return myText.getSolution();
 }
 
 int main() {
 
-	/*
-	vector<vector<string>> temp;
-	temp = parser();
-	for (auto i : temp) {
-		for (auto j : i) {
-			cout << j << endl;
-		}
-	}
-	*/
+	vector<string> stuffs = solution(parser());
 
-	vector<string> soln;
-	soln = solution(parser());
-
-	for (auto i : soln) {
-		cout << i << endl;
+	for (string stuff : stuffs) {
+		cout << stuff << endl;
 	}
-	
+
 	return 0;
 }
+
