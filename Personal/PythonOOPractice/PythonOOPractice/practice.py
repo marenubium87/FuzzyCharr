e037648c2106eb79dd7e335dbc2b6cedd85e3ee4
Aerie <marenubium87@gmail.com>
2022-07-20 15:43:33 -0600

    Finished HW3

--- Personal/PythonOOPractice/PythonOOPractice/practice.py
+++ Personal/PythonOOPractice/PythonOOPractice/practice.py
@@ -16,7 +16,30 @@
     def set_raise_amt(cls, amount):
         cls.raise_amount = amount
 
-aerie = Employee('A', 'E', 5)
-aerie.printAll()
+class Manager(Employee):
+    def __init__(self, first, last, pay, employees = None):
+        Employee.__init__(self, first, last, pay)
+        if employees is None:
+            self.employees = []
+        else:
+            self.employees = employees
 
-Employee.printAll(aerie)
+    def print_emps(self):
+        for emp in self.employees:
+            emp.printAll()
+    
+    def add_emp(self, emp):
+        if emp not in self.employees:
+            self.employees.append(emp)
+
+    def remove_emp(self, emp):
+        if emp in self.employees:
+            self.employees.remove(emp)
+
+dev_1 = Employee('Bob', 'Jenkins', 60000)
+dev_2 = Employee('Andrew', 'Grant', 70000)
+
+mgr_1 = Manager('Sue', 'Smith', 90000, [dev_1])
+mgr_1.add_emp(dev_2)
+
+mgr_1.print_emps()
