fbaeefeabb641c4d0b6ded7e90553435df65d3a0
marenubium87 <marenubium87@gmail.com>
2022-07-20 16:20:37 -0600

    Finished MA5

--- CptS_223/MAs/MA5/instrumentedMergeSort.h
+++ CptS_223/MAs/MA5/instrumentedMergeSort.h
@@ -17,29 +17,33 @@
 
 using namespace std;
 
-vector<int> mergeUs(vector<int> left, vector<int> right) {
+vector<int> mergeUs(vector<int> left, vector<int> right, SortStats & stats) {
     vector<int> result;
     int i = 0;
     int j = 0;
 
     //both lists still have entries
     while(i < left.size() && j < right.size()) {
-        if(left[i] <= right[j]) {
+        if(++stats.compares && left[i] <= right[j]) {
             result.push_back(left[i]);
+            stats.moves++;
             i++;
         }
         else {
             result.push_back(right[j]);
+            stats.moves++;
             j++;
         }
     }
 
     //only one list (at most) still has entries remaining at this point
     while(i < left.size()) {
+        stats.moves++;
         result.push_back(left[i]);
         i++;
     }
     while (j < right.size()) {
+        stats.moves++;
         result.push_back(right[j]);
         j++;
     }
@@ -47,27 +51,30 @@ vector<int> mergeUs(vector<int> left, vector<int> right) {
     return result;
 }
 
-vector<int> mergeSort(vector<int> & a, int left, int right) {
+vector<int> mergeSort(vector<int> & a, int left, int right, SortStats & stats) {
     vector<int> temp;
     
     //one and two element subvector situations
-    if(left == right) {
+    if(++stats.compares && left == right) {
+        stats.moves++;
         temp.push_back(a[left]);
     }
-    else if(right - left == 1) {
+    else if(++stats.compares && right - left == 1) {
+        stats.moves += 2;
         temp.push_back(a[left]);
         temp.push_back(a[right]);
-        if(temp[0] > temp[1]) {
+        if(++stats.compares && temp[0] > temp[1]) {
+            stats.moves++;
             swap(temp[0], temp[1]);
         }
     }
     else {
         //recursively mergesort left and right halves
-        vector<int> tempLeft = mergeSort(a, left, (left + right) / 2);
-        vector<int> tempRight = mergeSort(a, (left + right) / 2 + 1, right);
+        vector<int> tempLeft = mergeSort(a, left, (left + right) / 2, stats);
+        vector<int> tempRight = mergeSort(a, (left + right) / 2 + 1, right, stats);
 
         //recombine both lists
-        temp = mergeUs(tempLeft, tempRight);
+        temp = mergeUs(tempLeft, tempRight, stats);
     }
 
     return temp;
@@ -80,7 +87,7 @@ void instrumentedMergeSort( vector<int> & a, SortStats & stats )
 
 	// MA TODO: Implement Merge Sort plus logging compares and moves/swaps
 
-    a = mergeSort(a, 0, a.size() - 1);
+    a = mergeSort(a, 0, a.size() - 1, stats);
 
     clock_t time_end = clock();         // Grab time when sort complete
 	stats.sortTime = double(time_end - time_begin) / CLOCKS_PER_SEC;
